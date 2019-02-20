44063cc08dd671e835f01fc9307b99e6c2081dbd
Charles Michael Craven <charlesmcraven@students.abtech.edu>
2019-02-20 11:44:53 -0500

    commit to handle the .gitignore file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Asgn03</title>
+  <link href="../css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  <h1>Assignment 03</h1>
+  <ul>
+    <li><a href="cleaners.php">Assignment #4 Cleaners</a></li>
+  </ul>
+  <a href="../index.php">Home Page</a>
+</body>
+
+</html>
